e81e534ec2070a07850f08bbf7384d43fabf7fd1
Melvin <contactmelvinadu@gmail.com>
2022-04-10 19:07:59 -0400

    add truffle tests for whether the right eth addresses are being retrieved after deploying smart contract

--- test/album_sale.js
+++ test/album_sale.js
@@ -5,9 +5,20 @@ const AlbumSale = artifacts.require("AlbumSale");
  * Ethereum client
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
-contract("AlbumSale", function (/* accounts */) {
+contract("AlbumSale", function (accounts) {
   it("should assert true", async function () {
     await AlbumSale.deployed();
     return assert.isTrue(true);
   });
+
+  it("should get the right account owner account and charity account", async function () {
+    const contract = await AlbumSale.deployed();
+    
+    const owner = await contract.owner.call()
+    assert.isTrue(owner == 0xdd0001Cc2B6Cb99CA0CB56eFE61B82D8935Ee75F)
+
+    const charity = await contract.charity.call()
+    assert.isTrue(charity == 0x0206dDB656Da973230Bf332d7D34fEafc5A45E3c)
+  });
+
 });
