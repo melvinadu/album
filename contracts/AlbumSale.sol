294d54ebd85ddfb76f6e05a62e03726f694bd4a4
Melvin <contactmelvinadu@gmail.com>
2022-04-10 11:24:04 -0400

    make desired variables public information on the blockchain for transparency purposes

--- contracts/AlbumSale.sol
+++ contracts/AlbumSale.sol
@@ -4,11 +4,11 @@ pragma solidity >=0.4.22 <0.9.0;
 contract AlbumSale {
 
   // types in SOL referenced from https://docs.soliditylang.org/en/v0.8.13/types.html
-  // uint variables cannot be negative
-  uint totalSales;
-  uint maxSales;
+  // uint variables cannot be negative and are hex values which is why maxSales is showing as 64 on the blockchain
+  uint public totalSales;
+  uint public maxSales;
 
-  constructor() public {
+  constructor() {
     totalSales = 0;
     maxSales = 100;
   }
