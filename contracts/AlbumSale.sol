abd5b25981ac23c71eceb389d0a6ce25b16104a2
Melvin <contactmelvinadu@gmail.com>
2022-04-10 11:17:58 -0400

    start the setup of AlbumSale smart contract with total and max sales allowed

--- contracts/AlbumSale.sol
+++ contracts/AlbumSale.sol
@@ -2,6 +2,15 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract AlbumSale {
+
+  // types in SOL referenced from https://docs.soliditylang.org/en/v0.8.13/types.html
+  // uint variables cannot be negative
+  uint totalSales;
+  uint maxSales;
+
   constructor() public {
+    totalSales = 0;
+    maxSales = 100;
   }
+
 }
---
+++ sandbox/prism-sale-fake.js
@@ -0,0 +1,21 @@
+//this is how AlbumSale.sol would look in JavaScript
+
+// class AlbumSale {
+//   constructor() {
+//     this.totalSales = 0
+//     this.maxSales = 100
+//     this.owner = "0x..."
+//     this.charity = "0x..."
+//   }
+
+//   canBuy(){
+//     return this.totalSales < this.maxSales
+//   }
+
+//   hasAccess() {
+//   }
+
+//   buy() {
+//   }
+// }
+
